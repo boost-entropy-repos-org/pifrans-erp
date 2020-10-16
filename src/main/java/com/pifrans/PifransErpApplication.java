9685747c066aa0f9c557203c3433d85056828a36
Lucas Francisco <lucasf.silva@live.com>
2020-10-15 21:31:51 -0300

    Criação do projeto

---
+++ src/main/java/com/pifrans/PifransErpApplication.java
@@ -0,0 +1,13 @@
+package com.pifrans;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PifransErpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PifransErpApplication.class, args);
+	}
+
+}
