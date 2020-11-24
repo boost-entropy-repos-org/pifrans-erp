27bd34e3f155961d15cbaf73f656a9545ed24854
Lucas Francisco <lucasf.silva@live.com>
2020-11-24 18:30:25 -0300

    Implementação de novas classes services para places e finances

--- src/main/java/com/pifrans/modules/finances/services/CurrencyService.java
+++ src/main/java/com/pifrans/modules/finances/services/CurrencyService.java
@@ -1,11 +1,12 @@
 package com.pifrans.modules.finances.services;
 
-import java.io.Serializable;
-
 import org.springframework.stereotype.Service;
 
+import com.pifrans.global.services.GenericService;
+import com.pifrans.modules.finances.models.Currency;
+
 @Service
-public class CountryService implements Serializable {
+public class CurrencyService extends GenericService<Currency> {
 	private static final long serialVersionUID = 1L;
 	//private static final Logger LOG = Logger.getLogger(CountryService.class.getName());
 
---
+++ src/main/java/com/pifrans/modules/places/services/CityService.java
@@ -0,0 +1,14 @@
+package com.pifrans.modules.places.services;
+
+import org.springframework.stereotype.Service;
+
+import com.pifrans.global.services.GenericService;
+import com.pifrans.modules.places.models.City;
+
+@Service
+public class CityService extends GenericService<City> {
+	private static final long serialVersionUID = 1L;
+	// private static final Logger LOG =
+	// Logger.getLogger(PersonService.class.getName());
+
+}
