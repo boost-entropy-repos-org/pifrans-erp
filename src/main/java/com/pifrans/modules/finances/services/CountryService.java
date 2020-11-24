849df03bee9e4b7326ed573c51b2442df416182a
Lucas Francisco <lucasf.silva@live.com>
2020-11-24 18:23:36 -0300

    Atualização de CountryService

--- src/main/java/com/pifrans/modules/finances/services/CountryService.java
+++ src/main/java/com/pifrans/modules/finances/services/CountryService.java
@@ -1,13 +1,12 @@
 package com.pifrans.modules.finances.services;
 
 import java.io.Serializable;
-import java.util.logging.Logger;
 
 import org.springframework.stereotype.Service;
 
 @Service
 public class CountryService implements Serializable {
 	private static final long serialVersionUID = 1L;
-	private static final Logger LOG = Logger.getLogger(CountryService.class.getName());
+	//private static final Logger LOG = Logger.getLogger(CountryService.class.getName());
 
 }
