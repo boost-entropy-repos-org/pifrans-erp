04758cf4a72c1ba7dfe6f4059515f6423f966f1f
Lucas Francisco <lucasf.silva@live.com>
2020-11-24 21:49:27 -0300

    Reparo nas interfaces repositories de finances e alteração no application-test

--- src/main/java/com/pifrans/modules/finances/repositories/CategoryRepository.java
+++ src/main/java/com/pifrans/modules/finances/repositories/CategoryRepository.java
@@ -3,9 +3,9 @@ package com.pifrans.modules.finances.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.pifrans.modules.places.models.Address;
+import com.pifrans.modules.finances.models.Category;
 
 @Repository
-public interface CategoryRepository extends JpaRepository<Address, Long> {
+public interface CategoryRepository extends JpaRepository<Category, Long> {
 
 }
--- src/main/java/com/pifrans/modules/finances/repositories/CurrencyRepository.java
+++ src/main/java/com/pifrans/modules/finances/repositories/CurrencyRepository.java
@@ -3,9 +3,9 @@ package com.pifrans.modules.finances.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.pifrans.modules.places.models.Address;
+import com.pifrans.modules.finances.models.Currency;
 
 @Repository
-public interface CurrencyRepository extends JpaRepository<Address, Long> {
+public interface CurrencyRepository extends JpaRepository<Currency, Long> {
 
 }
